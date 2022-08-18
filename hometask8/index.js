73d6fd023d8093c6f0f94ef6b7973e81ae5019a8
NKateryna <krapiva.b.s@gmail.com>
2022-08-18 03:08:59 +0300

    JS3: hometask 8

--- hometask8/index.js
+++ hometask8/index.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 // Для эмуляции сервера будем использовать стороннюю библиотеку.
 // Для запуска необходимо в терминале запустить команду
 // npm install -g json-server для установки
@@ -12,6 +12,22 @@ const fetch = require('node-fetch');
  * 1. Сделать GET запрос на сервер за адресом http://localhost:3000/users
  * И вернуть только значимую часть запроса (Идентификатор, например, не выводится на экран, потому для обработки он может быть ненужен).
  */
+function UsersInfofetch() {
+  fetch("http://localhost:3000/users", {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      json.forEach((element) => {
+        delete element.id;
+      });
+      console.log(json);
+    });
+}
+
+UsersInfofetch();
 
 /**
  * 2. Сделать POST запрос на сервер за адресом http://localhost:3000/users
@@ -20,12 +36,41 @@ const fetch = require('node-fetch');
  * Всех пользователей и убедиться в том что новый пользователь добавлен
  */
 
+function postCheckfetch(userName, userSurname, userAge) {
+  fetch("http://localhost:3000/users", {
+    method: "POST",
+    body: JSON.stringify({
+      name: userName,
+      surname: userSurname,
+      age: userAge,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  }).then(() =>
+    fetch("http://localhost:3000/users")
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+  );
+}
+postCheckfetch("Maksim", `Citrusov`, 34);
+
 /**
  * 3. При помощи DELETE запроса на сервер за адресом http://localhost:3000/users
  * Удалить своего пользователя, которого создали в задани 2
  * После успешного запроса отправить новый запрос для того чтобы получить
  * Всех пользователей и убедиться в том что пользователь удален
  */
+function deleteCheckfetch() {
+  fetch("http://localhost:3000/users/3", {
+    method: "DELETE",
+  }).then(() =>
+    fetch("http://localhost:3000/users")
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+  );
+}
+deleteCheckfetch();
 
 /**
  * 4. Получить данные про пользователя за адресом http://localhost:3000/users/1 используя метод GET
@@ -33,3 +78,35 @@ const fetch = require('node-fetch');
  * После сделать запрос GET на сервер за адресом http://localhost:3000/users/1
  * И получить данные про измененного пользователя
  */
+
+function ageChangefetchUser1(newAge) {
+  fetch("http://localhost:3000/users/1", {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      let jsonNew = json;
+      jsonNew.age = newAge;
+
+      return jsonNew;
+    })
+    .then((jsonNew) =>
+      fetch("http://localhost:3000/users/1", {
+        method: "PUT",
+        body: JSON.stringify({ ...jsonNew }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+    )
+    .then(() =>
+      fetch("http://localhost:3000/users/1", {
+        method: "GET",
+      })
+    )
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
+
+ageChangefetchUser1(66);
