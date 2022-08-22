69318c077da7c67bf3d277062f25e710843ec81c
NKateryna <krapiva.b.s@gmail.com>
2022-08-22 15:04:21 +0300

    JS3: hometask 9

--- hometask9/index.js
+++ hometask9/index.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 // Для эмуляции сервера будем использовать стороннюю библиотеку.
 // Для запуска необходимо в терминале запустить команду
 // npm install -g json-server для установки
-// И json-server hometask8/server.json для запуска
+// И json-server hometask9/server.json для запуска
 
 // Для запуска кода использовать команды
 // npm install для установки всех зависимостей (достаточно установить их 1 раз, после код можно запускать)
@@ -11,7 +11,7 @@ const fetch = require("node-fetch");
 /**
  * 1. Сделать GET запрос на сервер за адресом http://localhost:3000/users
  * И вернуть только значимую часть запроса (Идентификатор, например, не выводится на экран, потому для обработки он может быть ненужен).
- * 
+ *
  * Переписать выполнение с использованием синтаксиса async/await
  */
 const BASE_URL = "http://localhost:3000";
@@ -25,16 +25,10 @@ function deleteInfo(array, ...keys) {
   return array;
 }
 
-function readUsers() {
-  fetch(`${BASE_URL}/users/`)
-    .then((response) => response.json())
-    .then((data) => {
-      deleteInfo(data, "id");
-      return data;
-    })
-    .then((data) => {
-      console.log(data);
-    });
+async function readUsers() {
+  const response = await fetch(`${BASE_URL}/users/`);
+  const users = await response.json();
+  console.log(deleteInfo(users, "id"));
 }
 
 readUsers();
@@ -44,23 +38,22 @@ readUsers();
  * Добавить пользователя со своим именем, фамилией и возрастом
  * После успешного запроса отправить новый запрос для того чтобы получить
  * Всех пользователей и убедиться в том что новый пользователь добавлен
- * 
+ *
  * Переписать выполнение с использованием синтаксиса async/await
  */
-
-function createUser(user = {}) {
-  fetch(`${BASE_URL}/users/`, {
+async function createUser(user = {}) {
+  await fetch(`${BASE_URL}/users/`, {
     method: "POST",
     body: JSON.stringify(user),
     headers: {
       "Content-type": "application/json",
     },
-  }).then(() =>
-    fetch(`${BASE_URL}/users/`)
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-  );
+  });
+  const response = await fetch(`${BASE_URL}/users/`);
+  const users = await response.json();
+  console.log(users);
 }
+
 createUser({
   name: "Maksim",
   surname: "Citrusov",
@@ -72,18 +65,19 @@ createUser({
  * Удалить своего пользователя, которого создали в задани 2
  * После успешного запроса отправить новый запрос для того чтобы получить
  * Всех пользователей и убедиться в том что пользователь удален
- * 
+ *
  * Переписать выполнение с использованием синтаксиса async/await
  */
-function deleteUsers(id) {
-  fetch(`${BASE_URL}/users/${id}`, {
+
+async function deleteUsers(id) {
+  await fetch(`${BASE_URL}/users/${id}`, {
     method: "DELETE",
-  }).then(() =>
-    fetch(`${BASE_URL}/users/`)
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-  );
+  });
+  const respone = await fetch(`${BASE_URL}/users/`);
+  const users = await respone.json();
+  console.log(users);
 }
+
 deleteUsers(3);
 
 /**
@@ -91,7 +85,7 @@ deleteUsers(3);
  * Запросом PUT на сервер за адресом http://localhost:3000/users/1 изменить возраст на 66
  * После сделать запрос GET на сервер за адресом http://localhost:3000/users/1
  * И получить данные про измененного пользователя
- * 
+ *
  * Переписать выполнение с использованием синтаксиса async/await
  */
 function transformedModel(odj, key, value) {
@@ -99,25 +93,20 @@ function transformedModel(odj, key, value) {
   return odj;
 }
 
-function updateUserAge(id, key, value) {
-  fetch(`${BASE_URL}/users/${id}`)
-    .then((response) => response.json())
-    .then((data) => {
-      transformedModel(data, key, value);
-      return data;
-    })
-    .then((data) =>
-      fetch(`${BASE_URL}/users/${id}`, {
-        method: "PUT",
-        body: JSON.stringify(data),
-        headers: {
-          "Content-type": "application/json",
-        },
-      })
-    )
-    .then(() => fetch(`${BASE_URL}/users/${id}`))
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+async function updateUserAge(id, key, value) {
+  const response = await fetch(`${BASE_URL}/users/${id}`);
+  const user = await response.json();
+  transformedModel(user, key, value);
+  await fetch(`${BASE_URL}/users/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(user),
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
+  const responseUpdate = await fetch(`${BASE_URL}/users/${id}`);
+  const userUpdate = await responseUpdate.json();
+  console.log(userUpdate);
 }
 
 updateUserAge(1, "age", 66);
